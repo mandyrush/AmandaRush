b5f0d1cedb871cf95f488b5224b2e790a625a28e
Amanda Rush <mandyrush85@gmail.com>
2019-07-15 23:15:51 -0600

    Create initial page design and routes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+import AboutMe from './views/AboutMe';
+import ContactMe from './views/ContactMe';
+import MyWork from './views/MyWork';
+
 Vue.use(Router);
 
 export default new Router({
@@ -9,16 +13,24 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: Home,
+      children: [
+        {
+          path: '/about-me',
+          name: 'about-me',
+          component: AboutMe
+        },
+        {
+          path: '/contact-me',
+          name: 'contact-me',
+          component: ContactMe
+        },
+        {
+          path: '/my-work',
+          name: 'my-work',
+          component: MyWork
+        }
+      ]
     }
   ]
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="page-content">
+      <div class="portfolio-img">
+          <img src="/amandaPic.jpg" alt="Image of Amanda">
+      </div>
+
+      <div class="header">
+          <div class="header-content">
+              <div class="title">About</div>
+              <div class="name"><span>Amanda Rush</span></div>
+          </div>
+
+          <Bio/>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Bio from "@/components/Bio.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+        Bio
   }
 };
 </script>
+
+<style lang="scss">
+    body {
+        margin: 0;
+    }
+    .page-content {
+        height: 100vh;
+        display: flex;
+    }
+    .portfolio-img {
+        flex-basis: 40%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 3rem;
+        img {
+            width: 75%;
+            height: auto;
+        }
+    }
+    .header {
+        flex-basis: 60%;
+        height: 100%;
+        display: grid;
+        grid-template-rows: 35% 65%;
+        .header-content {
+            position: relative;
+            height: 100%;
+        }
+    }
+    .title {
+        font-size: 15rem;
+        color: #f2f2f2;
+        position: absolute;
+        height: 100%;
+    }
+    .name {
+        font-size: 3rem;
+        color: rebeccapurple;
+        position: absolute;
+        height: 100%;
+        display: flex;
+        align-items: flex-end;
+    }
+
+    @media (max-width: 800px) {
+        .page-content {
+            flex-wrap: wrap;
+            padding: 1rem;
+        }
+        .portfolio-img {
+            flex-basis: 100%;
+            padding: 0;
+            justify-content: center;
+        }
+        .header {
+            flex-basis: 100%;
+            grid-template-rows: 20% 80%;
+        }
+        .title {
+            font-size: 7rem;
+        }
+    }
+</style>
