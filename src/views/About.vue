381cd04abe05166c9c61d0c177c5d9647d27c05a
Amanda Rush <mandyrush85@gmail.com>
2020-03-03 00:34:42 -0700

    Redesign about page and add parallax scrolling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,61 +1,107 @@
 <template>
-  <div class="description">
-    <p class="content is-large">
-      Greetings!"My name is Amanda.I am a front-end web developer with a passion
-      for technology, learning and creating. My main focus is on Javascript,
-      React, Vue, HTML5 and CSS3. I also have experience with Laravel and PHP."
-    </p>
-    <p>
-      <strong class="text-highlight">I am a</strong> self proclaimed writer,
-      traveler, artist, herder of cats and of course web developer. I spend my
-      days sipping coffee, writing code and bending to the will of my three
-      furry felines. In my spare time I love to go on adventures with my husband
-      and fellow developer Des, try out a new climbing wall or grab a drink with
-      friends.
-    </p>
+  <div class="about-me">
+    <div class="parallax parallax-one"></div>
 
-    <p>
-      <strong class="text-highlight">A few years ago</strong> I found myself
-      looking for a career change so I decided to give programming a try and
-      I’ve been hooked ever since. Until recently I worked remotely for a
-      company in North Carolina on the front end of a redesign project. Now that
-      my share of the project has come to completion, I am excited to explore
-      new opportunities where I can continue to learn and grow.
-    </p>
+    <div class="solid-block-one content-section">
+      <p>
+        "I am a self proclaimed writer, traveler, artist, herder of cats and of
+        course web developer! I spend my days sipping coffee, writing code and
+        bending to the will of my three furry felines. In my spare time I love
+        to go on adventures with my husband and fellow developer Des, try out a
+        new climbing wall or grab a drink with friends."
+      </p>
+    </div>
 
-    <p>
-      <strong class="text-highlight">I got my feet wet</strong> learning a
-      little about PHP, Laravel and MYSQL and put them to use building my first
-      project,
-      <a
-        href="http://catswander.com/"
-        target="_blank"
-        class="project-title link"
-        >catswander.com</a
-      >. As my learning continued I became interested in Javascript so I
-      switched gears and have been focusing on that ever since. I took several
-      courses on Vue, Vuex, React and Redux. I have completed projects using
-      both frameworks and the front end of my most recent project was a SPA
-      using Vue and Vuex. Currently I am diving into Node and Express to learn
-      how to build RESTFUL API's for a new project I am working on.
-    </p>
+    <div class="parallax parallax-two"></div>
 
-    <p>
-      <strong class="text-highlight">I love to learn</strong> and make a point
-      each day to set aside a little time dedicated to tackling new concepts. I
-      welcome any challenges and look forward to beginning my next projects.
-    </p>
+    <div class="solid-block-two content-section">
+      <p>
+        "Until recently I worked remotely for a company in North Carolina on the
+        front end of a redesign project. Now that my share of the project has
+        come to completion, I am excited to explore new opportunities where I
+        can continue to learn and grow."
+      </p>
+    </div>
 
-    <p>
-      <strong class="text-highlight">Please check out my links</strong> above
-      and feel free to send me a message if you would like to talk more about my
-      work. I can also be reached by phone or email. I'd love to hear from you!
-    </p>
+    <div class="parallax parallax-three"></div>
+
+    <div class="solid-block-three content-section">
+      <div>
+        <p>
+          "I love to learn and make a point each day to set aside a little time
+          dedicated to tackling new concepts. I welcome any challenges and look
+          forward to beginning my next projects.
+          <br />
+          Please check out my links above and feel free to send me a message if
+          you would like to talk more about my work.I'd love to hear from you!"
+        </p>
+        <div class="contact-btn">
+          <ContactButton />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import ContactButton from '../UI/ContactButton'
+export default {
+  components: {
+    ContactButton
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-.description p:not(:last-child) {
-  margin-bottom: 1.5rem;
+@import '../assets/sass/main.scss';
+.parallax {
+  min-height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.parallax-one {
+  background-image: url('../../public/test_image.jpg');
+}
+.parallax-two {
+  background-image: url('../../public/test_image2.jpg');
+}
+.parallax-three {
+  background-image: url('../../public/test_image3.jpg');
+}
+.solid-block-one {
+  min-height: 500px;
+  background-color: $dark;
+  color: $green;
+}
+.solid-block-two {
+  min-height: 500px;
+  background-color: $blue;
+  color: $dark;
+}
+.solid-block-three {
+  min-height: 500px;
+  background-color: $dark;
+  color: $pink;
+}
+.content-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5rem;
+  p {
+    font-size: 2rem;
+
+    text-align: center;
+  }
+}
+.contact-btn {
+  margin-top: 2rem;
+}
+@media only screen and (max-device-width: 1366px) {
+  .parallax {
+    background-attachment: scroll;
+  }
 }
 </style>
