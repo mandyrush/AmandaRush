590e3530f3e166ae4e8afa8364317db9f66cd110
Amanda Rush <mandyrush85@gmail.com>
2020-04-18 15:54:17 -0600

    Upgrade vue cli, fix whitespace

--- src/UI/ContactButton.vue
+++ src/UI/ContactButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="buttons is-centered">
     <a href="mailto:mandyrush85@gmail.com" class="button is-primary">
-      <span>Contact Me! &nbsp;</span><i class="fas fa-envelope"></i>
+      <span>Contact Me! <i class="fas fa-envelope"></i></span>
     </a>
   </div>
 </template>
